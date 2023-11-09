00b4cceca76decd5b2c33aef3b62964b2068e832
Klaas van Schelven <klaas@vanschelven.com>
2023-11-09 23:32:37 +0100

    Add test for parse_auth_header

--- ingest/tests.py
+++ ingest/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 
+from sentry.utils.auth import parse_auth_header
+
 
 class UtilsTestCase(TestCase):
-    def test_one(self):
-        self.asdf
+    def test_parse_header(self):
+        self.assertEquals(
+            {"sentry_key": "foo", "sentry_version": "bar"},
+            parse_auth_header('Sentry sentry_key=foo,sentry_version=bar'))
