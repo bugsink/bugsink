8af8ab921f1c84341a9bfd0a621604ff3a63c49e
Klaas van Schelven <klaas@vanschelven.com>
2023-11-17 17:05:19 +0100

    Show/hide all

--- static/js/issue.js
+++ static/js/issue.js
@@ -2,7 +2,6 @@
 
 function toggleFrameVisibility(frameHeader) {
     const frameDetails = frameHeader.parentNode.querySelector(".js-frame-details");
-    console.log("FD", frameDetails);
     if (frameDetails.classList.contains("hidden")) {
         frameDetails.classList.remove("hidden");
         frameDetails.classList.add("xl:flex");  // add back
@@ -10,4 +9,31 @@ function toggleFrameVisibility(frameHeader) {
         frameDetails.classList.add("hidden");
         frameDetails.classList.remove("xl:flex");  // this appears to be necessary, not sure why
     }
+}
+
+
+function showAllFrames(frameHeader) {
+    document.querySelectorAll(".js-frame-details").forEach((frameDetails) => {
+        frameDetails.classList.remove("hidden");
+        frameDetails.classList.add("xl:flex");
+    });
+}
+
+function showInAppFrames(frameHeader) {
+    document.querySelectorAll(".js-frame-details").forEach((frameDetails) => {
+        if (frameDetails.classList.contains("js-in-app")) {
+            frameDetails.classList.remove("hidden");
+            frameDetails.classList.add("xl:flex");  // add back
+        } else {
+            frameDetails.classList.add("hidden");
+            frameDetails.classList.remove("xl:flex");  // this appears to be necessary, not sure why
+        }
+    });
+}
+
+function hideAllFrames(frameHeader) {
+    document.querySelectorAll(".js-frame-details").forEach((frameDetails) => {
+        frameDetails.classList.add("hidden");
+        frameDetails.classList.remove("xl:flex");  // this appears to be necessary, not sure why
+    });
 }
