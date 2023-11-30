e4ef92ced577825f23b84f32fb98df01159fc85e
Klaas van Schelven <klaas@vanschelven.com>
2023-11-30 17:07:29 +0100

    2 thoughts documented

--- issues/regressions.py
+++ issues/regressions.py
@@ -1,10 +1,12 @@
 def is_regression(sorted_releases, fixed_at, events_at, current_event_at):
     # NOTE: linear in time with the number of releases; however, for now it's a nice reference implementation.
     # premature ... and the root of all evil. some thoughts though:
+    #
+    # "if current_event_at in events_at" return False <= this could be a shortcut
     # * sorted_releases grows with time for projects, but how many 'fixed_at` can we reasonably expect?
     #       unless we even do things like release cleanup, which isn't so crazy...
     # * we need not consider anything (from sorted_releases) before the first `fixed_at` moment, because that's the
-    #   first flipping of `marked_as_resolved`
+    #   first flipping of `marked_as_resolved`. this could even be done at the DB level.
     #
     marked_as_resolved = False
 
