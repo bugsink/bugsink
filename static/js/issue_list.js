7fc2e121436d0eca41b0c74c4fcfd820810ca84f
Klaas van Schelven <klaas@vanschelven.com>
2024-02-20 19:19:45 +0100

    Implement (de)select all

--- static/js/issue_list.js
+++ static/js/issue_list.js
@@ -1,6 +1,14 @@
 "use strict";
 
-function toggleContainedCheckbox(td) {
-    const checkbox = td.querySelector("[type=\"checkbox\"]");
+function toggleContainedCheckbox(circleDiv) {
+    const checkbox = circleDiv.querySelector("[type=\"checkbox\"]");
     checkbox.checked = !checkbox.checked;
+}
+
+function matchIssueCheckboxesStateToMain(elementContainingMainCheckbox) {
+    const mainCheckbox = elementContainingMainCheckbox.querySelector("[type=\"checkbox\"]");
+    const checkboxes = document.querySelectorAll(".js-issue-checkbox");
+    for (let i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = mainCheckbox.checked;
+    }
 }
