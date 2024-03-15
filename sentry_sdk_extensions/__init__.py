52bb5a253b56efd8006f70351f188809a4004cac
Klaas van Schelven <klaas@vanschelven.com>
2024-03-15 20:36:10 +0100

    Introduce capture_stacktrace_2

--- sentry_sdk_extensions/__init__.py
+++ sentry_sdk_extensions/__init__.py
@@ -2,6 +2,10 @@ from sentry_sdk.utils import capture_internal_exceptions, current_stacktrace
 import sentry_sdk
 
 
+class AdHocException(Exception):
+    pass
+
+
 def capture_stacktrace(message):
     """
     Capture the current stacktrace and send it to Sentry; the standard sentry_sdk does not provide this; it either
@@ -26,3 +30,17 @@ def capture_stacktrace(message):
     event["level"] = "error"
     event["logentry"] = {"message": message}
     sentry_sdk.capture_event(event)
+
+
+def capture_stacktrace_2(message):
+    """
+    Capture the current stacktrace and send it to Sentry. Like capture_stacktrace, but using an "Ad Hoc" Exception.
+    This allows us to use the standard sentry_sdk.capture_exception function, and our stacktrace to show up as an
+    exception on the server-side.
+
+    In good old Python tradition, we've just tacked a "_2" onto the end of the function name.
+    """
+    try:
+        raise AdHocException(message)
+    except AdHocException as e:
+        sentry_sdk.capture_exception(e)
