2eac8990b15c1af6655cbebe716e92383fe0e62c
Klaas van Schelven <klaas@vanschelven.com>
2024-03-15 20:05:04 +0100

    Promote settings-level 'hack' to command-param

--- ingest/management/commands/raise_exception.py
+++ ingest/management/commands/raise_exception.py
@@ -1,10 +1,24 @@
+from sentry_sdk.hub import GLOBAL_HUB
+
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     help = "Quick and dirty command to just raise an exception and see it show up in Bugsink"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--no-release",
+            action="store_true",
+            help="Set the value of the sent release to None",
+        )
+
     def handle(self, *args, **options):
+        if options["no_release"]:
+            # The sentry client "tries hard" to get the release from the environment (including git); I have found the
+            # following to be a workable way to set the release to None
+            GLOBAL_HUB.client.options['release'] = None
+
         try:
             self.raise_exception("Exception raised on purpose - 2")
         except Exception as e:
