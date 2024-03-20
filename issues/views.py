525fbee39021c886289a6152c6940d7f4fcfc8e0
Klaas van Schelven <klaas@vanschelven.com>
2024-03-20 20:41:08 +0100

    validate against illegal actions

--- issues/views.py
+++ issues/views.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
 import json
+
 from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 
 from events.models import Event
 
@@ -24,6 +26,48 @@ GLOBAL_MUTE_OPTIONS = [
     MuteOption("until", "hour", 24, 5),
     MuteOption("until", "hour", 24, 100),
 ]
+
+
+def _is_valid_action(action, issue):
+    """We take the 'strict' approach of complaining even when the action is simply a no-op, because you're already in
+    the desired state."""
+
+    if issue.is_resolved:
+        # any action is illegal on resolved issues (as per our current UI)
+        return False
+
+    if action.startswith("resolved_release:"):
+        release_version = action.split(":", 1)[1]
+        if issue.events_at.contains(release_version + "\n"):
+            return False
+
+    elif action.startswith("mute"):
+        if issue.is_muted:
+            return False
+
+    elif action == "unmute":
+        if not issue.is_muted:
+            return False
+
+    return True
+
+
+def _q_for_invalid_for_action(action):
+    """returns a Q obj of issues for which the action is not valid."""
+
+    illegal_conditions = Q(is_resolved=True)  # any action is illegal on resolved issues (as per our current UI)
+
+    if action.startswith("resolved_release:"):
+        release_version = action.split(":", 1)[1]
+        illegal_conditions = illegal_conditions | Q(events_at__contains=release_version + "\n")
+
+    elif action.startswith("mute"):
+        illegal_conditions = illegal_conditions | Q(is_muted=True)
+
+    elif action == "unmute":
+        illegal_conditions = illegal_conditions | Q(is_muted=False)
+
+    return illegal_conditions
 
 
 def _apply_action(manager, issue_or_qs, action):
@@ -60,7 +104,15 @@ def issue_list(request, project_id, state_filter="open"):
     if request.method == "POST":
         issue_ids = request.POST.getlist('issue_ids[]')
         issue_qs = Issue.objects.filter(pk__in=issue_ids)
-        _apply_action(IssueQuerysetStateManager, issue_qs, request.POST["action"])
+        illegal_conditions = _q_for_invalid_for_action(request.POST["action"])
+        # list() is necessary because we need to evaluate the qs before any actions are actually applied (if we don't,
+        # actions are always marked as illegal, because they are applied first, then checked (and applying twice is
+        # illegal)
+        unapplied_issue_ids = list(issue_qs.filter(illegal_conditions).values_list("id", flat=True))
+        _apply_action(IssueQuerysetStateManager, issue_qs.exclude(illegal_conditions), request.POST["action"])
+
+    else:
+        unapplied_issue_ids = None
 
     d_state_filter = {
         "open": lambda qs: qs.filter(is_resolved=False, is_muted=False),
@@ -82,6 +134,8 @@ def issue_list(request, project_id, state_filter="open"):
         "state_filter": state_filter,
         "mute_options": GLOBAL_MUTE_OPTIONS,
 
+        "unapplied_issue_ids": unapplied_issue_ids,
+
         # design decision: we statically determine some disabledness (i.e. choices that will never make sense are
         # disallowed), but we don't have any dynamic disabling based on the selected issues.
         "disable_resolve_buttons": state_filter in ("resolved"),
@@ -101,8 +155,16 @@ def issue_event_detail(request, issue_pk, event_pk):
     issue = get_object_or_404(Issue, pk=issue_pk)
 
     if request.method == "POST":
-        _apply_action(IssueStateManager, issue, request.POST["action"])
-        issue.save()
+        if _is_valid_action(request.POST["action"], issue):
+            _apply_action(IssueStateManager, issue, request.POST["action"])
+            issue.save()
+
+        # note that if the action is not valid, we just ignore it (i.e. we don't show any error message or anything)
+        # this is probably what you want, because the most common case of action-not-valid is 'it already happened
+        # through some other UI path'. The only case I can think of where this is not the case is where you try to
+        # resolve an issue for a specific release, and while you where thinking about that, it occurred for that
+        # release. In that case it will probably stand out that your buttons don't become greyed out, and that the
+        # dropdown no longer functions.
         return redirect(issue_event_detail, issue_pk=issue_pk, event_pk=event_pk)
 
     event = get_object_or_404(Event, pk=event_pk)
