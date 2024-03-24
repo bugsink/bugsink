b9616f9621349c68fd6050113339fe0d1157aaaa
Klaas van Schelven <klaas@vanschelven.com>
2024-03-24 22:08:38 +0100

    Hook make_consistent() into debug_server reloads

--- events/apps.py
+++ events/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from django.utils.autoreload import autoreload_started
+
+
+def watch_for_debugserver_reload(sender, **kwargs):
+    from .management.commands.make_consistent import make_consistent
+    make_consistent()
 
 
 class EventsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'events'
+
+    def ready(self):
+        autoreload_started.connect(watch_for_debugserver_reload)
--- events/management/commands/make_consistent.py
+++ events/management/commands/make_consistent.py
@@ -7,6 +7,34 @@ from issues.models import Issue
 from events.models import Event
 
 
+def make_consistent():
+    # Delete all issues that have no events; they should not exist and in fact break various templates and views
+    for issue in Issue.objects.annotate(fresh_event_count=Count('event')).filter(fresh_event_count=0):
+        print("Deleting issue %s, because it has 0 events" % issue)
+        issue.delete()
+
+    # Cleanup of dangling stuff (e.g. events without issues); not harmful, but not useful either
+    for issue in Issue.objects.filter(project=None):
+        print("Deleting issue %s, because it has no project" % issue)
+        issue.delete()
+
+    for release in Release.objects.filter(project=None):
+        print("Deleting release %s, because it has no project" % release)
+        release.delete()
+
+    for event in Event.objects.filter(issue=None):
+        print("Deleting event %s, because it has no issue" % event)
+        event.delete()
+
+    for event in Event.objects.filter(project=None):
+        print("Deleting event %s, because it has no project" % event)
+        event.delete()
+
+    for decompressed_event in DecompressedEvent.objects.filter(project=None):
+        print("Deleting decompressed event %s, because it has no project" % decompressed_event)
+        decompressed_event.delete()
+
+
 class Command(BaseCommand):
     help = """In 'normal operation', this command should not be used, because normal operation leaves the DB in a
 consistent state. However, during development all bets are off, and to get back to sanity this command may be used."""
@@ -15,28 +43,4 @@ consistent state. However, during development all bets are off, and to get back 
         pass
 
     def handle(self, *args, **options):
-        # Delete all issues that have no events; they should not exist and in fact break various templates and views
-        for issue in Issue.objects.annotate(fresh_event_count=Count('event')).filter(fresh_event_count=0):
-            print("Deleting issue %s, because it has 0 events" % issue)
-            issue.delete()
-
-        # Cleanup of dangling stuff (e.g. events without issues); not harmful, but not useful either
-        for issue in Issue.objects.filter(project=None):
-            print("Deleting issue %s, because it has no project" % issue)
-            issue.delete()
-
-        for release in Release.objects.filter(project=None):
-            print("Deleting release %s, because it has no project" % release)
-            release.delete()
-
-        for event in Event.objects.filter(issue=None):
-            print("Deleting event %s, because it has no issue" % event)
-            event.delete()
-
-        for event in Event.objects.filter(project=None):
-            print("Deleting event %s, because it has no project" % event)
-            event.delete()
-
-        for decompressed_event in DecompressedEvent.objects.filter(project=None):
-            print("Deleting decompressed event %s, because it has no project" % decompressed_event)
-            decompressed_event.delete()
+        make_consistent()
