2f28dac76642bedf74b53021634532dc11f52b67
Klaas van Schelven <klaas@vanschelven.com>
2024-03-24 20:58:30 +0100

    '|items' as a templatefilter

---
+++ theme/templatetags/stricter_templates.py
@@ -0,0 +1,22 @@
+from django import template
+register = template.Library()
+
+
+@register.filter(name='items')
+def items(value):
+    """Just do value.items(); the reason to not just do value.items in the templatte that the latter will first do a
+    dictionary lookup; if the dictionary contains an item with the key 'items', that will be returned which is
+    definitely not what we want here. """
+    return value.items()
+
+
+# the general version below doesn't work, because we don't auto-append function-execution for callables and possibly a
+# whole bunch of other magic that Django does and we don't think about by default. I left it in as a starting point for
+# a possible future, but for now I'm going with the thing we actually need (above)
+# @register.filter(name='getattr')
+# def _getattr(value, arg):
+#     """Just do getattr(value, arg); the reason to not just do value.arg is that the latter will first do a dictionary
+#     lookup; if the dictionary contains an item with the key 'arg', that will be returned, even if there is a
+#     method/attribute of the same name.
+#     """
+#     return getattr(value, arg)
