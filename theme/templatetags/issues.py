12bbde947af09ce29b5f8496dc6f474ea165b48c
Klaas van Schelven <klaas@vanschelven.com>
2024-03-29 17:29:06 +0100

    Use pygments, the simple way
    as per their quickstart

--- theme/templatetags/issues.py
+++ theme/templatetags/issues.py
@@ -1,5 +1,18 @@
 from django import template
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+from django.utils.safestring import mark_safe
+
+
 register = template.Library()
+
+
+@register.filter
+def pygmentize(value):
+    # first, get the actual code from the frame
+    code = "\n".join(value['pre_context'] + [value['context_line']] + value['post_context'])
+    return(mark_safe(highlight(code, PythonLexer(), HtmlFormatter())))
 
 
 @register.filter(name='firstlineno')
