7caf6bbac45dbbc61c6a9e0fca580cd21341248b
Klaas van Schelven <klaas@vanschelven.com>
2024-04-05 15:50:06 +0200

    docstring

--- sentry/eventtypes/base.py
+++ sentry/eventtypes/base.py
@@ -3,6 +3,9 @@ from sentry.utils.strings import strip, truncatechars
 
 
 class DefaultEvent:
+    """The DefaultEvent is the Event for which there is no exception set. Given the implementation of `get_title`, I'd
+    actually say that this is basically the LogMessageEvent.
+    """
 
     def get_title(self, data):
         message = strip(
@@ -12,4 +15,5 @@ class DefaultEvent:
 
         if message:
             return truncatechars(message.splitlines()[0], 100)
+
         return "<unlabeled event>"
