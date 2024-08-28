ea5ac848fc37211ec1c7b1d256c3e76ac609b556
Klaas van Schelven <klaas@vanschelven.com>
2024-08-28 14:23:28 +0200

    Email backend: don't barf on the screen

---
+++ bugsink/email_backends.py
@@ -0,0 +1,10 @@
+from django.core.mail.backends.console import EmailBackend as ConsoleEmailBackend
+
+
+class QuietConsoleEmailBackend(ConsoleEmailBackend):
+
+    def write_message(self, message):
+        msg = message.message()
+        # self.stream.write("From: %s\n" % msg["From"])
+        # self.stream.write("To: %s\n" % msg["To"])
+        self.stream.write("Mail not sent (not SMTP configured); Subject: %s\n" % msg["Subject"])
