7616858fcf482311540dc015bff492775b283058
Klaas van Schelven <klaas@vanschelven.com>
2024-08-28 08:42:22 +0200

    server-unified: terminate children on self-failure, deal with child-start-problems

--- bugsink/scripts/server_unified.py
+++ bugsink/scripts/server_unified.py
@@ -29,13 +29,34 @@ class ParentProcess:
         signal.signal(signal.SIGINT, self.signal_handler)
         signal.signal(signal.SIGTERM, self.signal_handler)
 
+        try:
+            self.start_children()
+            self.connect_childrens_fates()
+        finally:
+            for child in self.children:
+                child.wait()
+
+    def start_children(self):
         # Start the server
         # Leaving stdout and stderr as None will make the output of the child processes be passed as our own.
         for args in self.parse_args():
-            child = subprocess.Popen(args)
+            try:
+                child = subprocess.Popen(args)
+            except Exception:
+                # Print is a bit superflous here, as the exception will be printed anyway by the raise
+                # print("Server-unified failed to start process:", " ".join(args), "(%s)" % e)
+                self.terminate_children()
+                raise
+
             print("Server-unified started process %s:" % child.pid, " ".join(args))
             self.children.append(child)
 
+    def terminate_children(self, except_child=None):
+        for child in self.children:
+            if child != except_child:
+                child.send_signal(signal.SIGTERM)
+
+    def connect_childrens_fates(self):
         # Check if any of the children have exited
         children_are_alive = True
         while children_are_alive:
@@ -45,13 +66,7 @@ class ParentProcess:
                 if child.poll() is not None:
                     # One of the children has exited
                     children_are_alive = False
-
-                    for other_child in self.children:
-                        if other_child != child:
-                            other_child.send_signal(signal.SIGTERM)
-
-        for child in self.children:
-            child.wait()
+                    self.terminate_children(except_child=child)
 
     def parse_args(self):
         """Splits our own arguments into a list of args for each of the children each, we split on "UNIFIED_WITH"."""
