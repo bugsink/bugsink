0f7e174499c5aae4899a889a2f2a2042fa8ac652
Klaas van Schelven <klaas@vanschelven.com>
2024-10-04 09:20:26 +0200

    'reminder about build-wheel-first

--- buildxdocker.bash
+++ buildxdocker.bash
@@ -4,6 +4,13 @@ set -e
 WHEEL_FILE=$(ls dist/ -1atr | grep bugsink | grep whl | tail -n1)
 echo "Building docker image with wheel file: $WHEEL_FILE"
 
+echo "Is this the correct wheel file? [y/n]"
+read -r response
+if [[ ! "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    echo "Please run the script again with the correct wheel file in the dist/ directory"
+    exit 1
+fi
+
 # example: bugsink-0.1.8.dev5+g200ea5e.d20240827-py3-none-any.whl
 
 # if this a non-dev version, we tag the image with the full version number, major.minor, major, and latest
