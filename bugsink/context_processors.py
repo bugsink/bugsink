47bf207db5af0b309392d20fd97489c9584c0a4a
Klaas van Schelven <klaas@vanschelven.com>
2024-11-11 14:04:50 +0100

    Nudge non-paying users to paid (yellow banner, site-title)

--- bugsink/context_processors.py
+++ bugsink/context_processors.py
@@ -1,11 +1,27 @@
+from datetime import timedelta
+
+from django.utils import timezone
 from bugsink.app_settings import get_settings, CB_ANYBODY
+
+from phonehome.models import Installation
 
 
 def useful_settings_processor(request):
+    installation = Installation.objects.get()
+
+    nag_7 = installation.created_at < timezone.now() - timedelta(days=7)
+    nag_30 = installation.created_at < timezone.now() - timedelta(days=30)
+
     return {
-        'site_title': get_settings().SITE_TITLE,
+        # Note: no way to actually set the license key yet, so nagging always happens for now.
+        'site_title': get_settings().SITE_TITLE + (" (non-production use)" if nag_7 else ""),
         'registration_enabled': get_settings().USER_REGISTRATION == CB_ANYBODY,
         'app_settings': get_settings(),
+
+        # (First version of "should I nag" logic): nag only after considerable time to play with the app, and for "some
+        # indication" that you're using this in production (the simplest such indication is that you've configured a
+        # BASE_URL that's not localhost). Subject to change.
+        'show_free_version_message': nag_30 and '127.0.0.1' not in get_settings().BASE_URL,
     }
 
 
--- phonehome/models.py
+++ phonehome/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Installation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class OutboundMessage(models.Model):
