0f60dab032cac8817079d189704aad0d1e3989f4
Klaas van Schelven <klaas@vanschelven.com>
2024-11-11 14:37:27 +0100

    phone-home: on (docker) server-start too

--- ingest/management/commands/prestart.py
+++ ingest/management/commands/prestart.py
@@ -3,6 +3,8 @@ import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
+from phonehome.tasks import send_if_due
+
 
 User = get_user_model()
 
@@ -20,3 +22,9 @@ class Command(BaseCommand):
             if not User.objects.filter(username=username).exists():
                 User.objects.create_superuser(username=username, password=password)
                 print(f"Superuser created: {username}")
+
+        # Similar considerations apply here as those which are documented in bugsink.views._phone_home().
+        # By putting this in prestart, we add one more location to the list of kick-off locations; with the added
+        # benefit that this particular location also gives some signal for (Docker) installations that are prematurely
+        # aborted (i.e. we get a ping even if 'home' is never even reached).
+        send_if_due.delay()
