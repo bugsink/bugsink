17e600d942d1cd9bac15d4eaec3b64589d0dc95b
Klaas van Schelven <klaas@vanschelven.com>
2024-11-22 10:22:29 +0100

    Typo in log-message

--- bugsink/email_backends.py
+++ bugsink/email_backends.py
@@ -7,4 +7,4 @@ class QuietConsoleEmailBackend(ConsoleEmailBackend):
         msg = message.message()
         # self.stream.write("From: %s\n" % msg["From"])
         # self.stream.write("To: %s\n" % msg["To"])
-        self.stream.write("Mail not sent (not SMTP configured); Subject: %s\n" % msg["Subject"])
+        self.stream.write("Mail not sent (no SMTP configured); Subject: %s\n" % msg["Subject"])
