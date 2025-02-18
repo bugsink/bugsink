3cb07246b97c1ff0308f2ff70a4ecdf75fdb2c99
Klaas van Schelven <klaas@vanschelven.com>
2025-02-18 12:08:05 +0100

    Add system-check for EVENT_STORAGES setting

--- bsmain/__init__.py
+++ bsmain/__init__.py
@@ -2,6 +2,7 @@ from django.core.checks import Warning, register
 from django.conf import settings
 
 from bugsink.app_settings import get_settings
+from events.storage_registry import get_write_storage
 
 
 @register("bsmain")
@@ -12,5 +13,20 @@ def check_no_nested_settings_in_unnested_form(app_configs, **kwargs):
             errors.append(Warning(
                 f"The setting {key} is defined at the top level of your configuration. It must be nested under the "
                 f"'BUGSINK' setting."
+            ))
+    return errors
+
+
+@register("bsmain")
+def check_event_storage_properly_configured(app_configs, **kwargs):
+    errors = []
+    try:
+        #  rather than doing an explicit check, we just run the `get_write_storage` code and see if it throws an error
+        # get_write_storage() touches the whole storage system, so if it fails, we know something is wrong
+        get_write_storage()
+    except ValueError as e:
+        errors.append(Warning(
+            str(e),
+            id="bsmain.W002",
             ))
     return errors
