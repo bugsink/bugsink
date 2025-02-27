ab961d81c14779c7e864843c15577f8c43960ed5
Klaas van Schelven <klaas@vanschelven.com>
2025-02-27 14:35:59 +0100

    init_tags: interruptable; show progress

--- tags/management/commands/init_tags.py
+++ tags/management/commands/init_tags.py
@@ -1,3 +1,6 @@
+import time
+import signal
+
 from django.core.management.base import BaseCommand
 
 from bugsink.transaction import immediate_atomic
@@ -9,12 +12,32 @@ class Command(BaseCommand):
     help = """Read all Tag-related data from the Events in the database and update the Tags in the DB accordingly."""
 
     def handle(self, *args, **options):
-        for event in Event.objects.all():
-            # transaction per event: allows for interrupting the process without losing all progress; also allows for
-            # your server to remain responsive during the process
-            with immediate_atomic():
-                # "in principle" we should refetch event here because we get a fresh transaction; in practice we only
-                # read from the event, and the event_data (the part we care about) is write-once anyway (unchanging
-                # after digest).
+        self.stopped = False
+        signal.signal(signal.SIGINT, self.handle_sigint)
 
-                digest_tags(event.get_parsed_data(), event, event.issue)
+        total = Event.objects.count()
+        t0 = time.time()
+
+        try:
+            for i, event in enumerate(Event.objects.all()):
+                # transaction per event: allows for interrupting the process without losing all progress; also allows
+                # for your server to remain responsive during the process
+                with immediate_atomic():
+                    # "in principle" we should refetch event here because we get a fresh transaction; in practice we
+                    # only read from the event, and the event_data (the part we care about) is write-once anyway
+                    # (unchanging after digest).
+
+                    digest_tags(event.get_parsed_data(), event, event.issue)
+
+                if (i + 1) % 1_000 == 0:
+                    print(f"Processed {i + 1}/{total} events")
+
+                if self.stopped:
+                    break
+
+        finally:
+            print(f"Processed {i + 1}/{total} events in {time.time() - t0:.2f}s at "
+                  f"{i / (time.time() - t0):.2f} events/s")
+
+    def handle_sigint(self, signum, frame):
+        self.stopped = True
