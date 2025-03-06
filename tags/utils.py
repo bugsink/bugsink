994e218e27884e5cc44a213593b1f1e1ae923c5b
Klaas van Schelven <klaas@vanschelven.com>
2025-03-06 15:58:54 +0100

    Notes on not-implemented tags

--- tags/utils.py
+++ tags/utils.py
@@ -7,7 +7,12 @@ from issues.utils import get_main_exception
 EVENT_DATA_CONVERSION_TABLE = {
     # "level" is not included here; Sentry puts this front and center for the tags; although we give it a non-prominent
     # place in the UI for the event-detail, Bugsink's take is that "level: Error" in an Error Tracker is not useful
-    # enough to warrant display-as-tag, nor even is it a searchable term.
+    # enough to warrant display-as-tag. Also, for tags you'd generally get 100% identical values for all events in a
+    # single issue, so it's not useful for tag-breakdown either. Arguably, for search, a case could be made,
+    # _especially_ if you treat Bugsink as a Log Aggregator. However, the (current) position of Bugsink is that we're an
+    # Error Tracker first and foremost, and that support for Log messages is only there to not confuse our users (i.e.
+    # we should always display the information that we have on hand). But searching for "info-level only" is not a
+    # use-case that we're actively supporting.
     # "level": ("level",),
 
     "server_name": ("server_name",),
@@ -30,9 +35,19 @@ CONTEXT_CONVERSION_TABLE = {
     "os.name": ("os", "name"),
     "os.version": ("os", "version"),
 
-    # TODO probably useful, simply not done yet:
+    # TODO maybe useful, but not sure yet; Sentry/GlitchTip give a very prominent place to the "runtime" tag (even an
+    # icon in the UI); but it's not clear to me that this is actually useful information. I _know_ that my code is
+    # running in language X, right? And I'd generally say "1 project => 1 language" so filtering isn't useful either.
+    # Regarding the version, I'd say that's even less useful (famous last words) because how many _bugs_ make it past
+    # CI/CD and are still runtime-specific? And even if they are, is it really more likely that you discover that fact
+    # by looking at the tag-breakdown of the runtime version than that you'd discover it by drilling down in the
+    # stacktrace?
     # runtime
     # runtime.name
+
+    # TODO Device-specific tags are probably useful, because they imply a world where you have your software running on
+    # many different devices, and you want to know if a bug is specific to a certain device. But I'd rather get a good
+    # use-case for this first (user-supplied).
     # device.something
 }
 
@@ -105,10 +120,21 @@ def deduce_tags(event_data):
     if user_tag := deduce_user_tag(contexts):
         tags["user"] = user_tag
 
-    # TODO further tags that are probably useful:
+    # TODO url is probably useful, but I imagine that its `mostly_unique` property is not statically known, i.e. some
+    # issues may have single url, others may have a few (useful for tag-breakdown) and yet others may have very many
+    # (useful for search). We'll tie implementation of this to the implementation of dynamic `is_mostly_unique`
+    # determination.
     # url
+
+    # TODO For now this is not supported for the same reason as "level" (see above). But it's probably more useful than
+    # level, because it will be more likely be a searchable term that "leads somewhere" (i.e. if you know you have a
+    # problem in module X, you can search for it). It's still somewhat annoying to implement (at least if you do the
+    # full fallback for all data-layouts) because the API has changed quite a bit recently (see `issues/views.py`)
     # logger
-    # mechanism
+
+    # Is mechanism useful? For search, probably not (I'm happy to be proven wrong with a specific use-case). For
+    # tag-breakdown, even less likely (it would my guess that the values would always be the same for a single issue).
+    # mechanism.type
 
     return tags
 
