1001726f4389e982c486cdd5fa81941cb46cfc33
Klaas van Schelven <klaas@vanschelven.com>
2025-07-29 14:50:16 +0200

    Ingestion: ensure event_id is a dashless uuid before using as a filename
    (cherry picked from commit b8b179ff948f852dc85e67851d625b05e183387d)

--- ingest/filestore.py
+++ ingest/filestore.py
@@ -1,3 +1,4 @@
+import uuid
 import os
 from bugsink.app_settings import get_settings
 
@@ -7,4 +8,7 @@ def get_filename_for_event_id(event_id):
     # implemented. However, counterpoint: when doing stress tests, it was quite hard to get a serious backlog going
     # (snappea was very well able to play catch-up). So this might not be necessary.
 
-    return os.path.join(get_settings().INGEST_STORE_BASE_DIR, event_id)
+    # ensure that event_id is a uuid, and remove dashes if present
+    event_id_normalized = uuid.UUID(event_id).hex
+
+    return os.path.join(get_settings().INGEST_STORE_BASE_DIR, event_id_normalized)
