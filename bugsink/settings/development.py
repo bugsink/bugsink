89db6d2899dc5bc761d1d6bfbcc02728849e7b67
Klaas van Schelven <klaas@vanschelven.com>
2025-07-30 14:00:39 +0200

    Configure bandit for 'spoils' usage
    i.e. B380: No os.path.join misuse.
    
    See https://github.com/bugsink/spoils
    
    rather than think-carefully-and-explain-with-nosec, just switch to
    safe_join: this saves future readers the pain of validating whether
    all assumptions are (still) correct at a (small) performance cost.
    
    See #175

--- bugsink/settings/development.py
+++ bugsink/settings/development.py
@@ -3,6 +3,7 @@ from .default import BASE_DIR, LOGGING, DATABASES, I_AM_RUNNING
 
 import os
 
+from django.utils._os import safe_join
 from sentry_sdk_extensions.transport import MoreLoudlyFailingTransport
 
 from bugsink.utils import deduce_allowed_hosts, eat_your_own_dogfood
@@ -112,7 +113,7 @@ if not I_AM_RUNNING == "TEST":
         "local_flat_files": {
             "STORAGE": "events.storage.FileEventStorage",
             "OPTIONS": {
-                "basepath": os.path.join(BASE_DIR, "filestorage"),
+                "basepath": safe_join(BASE_DIR, "filestorage"),
             },
             "USE_FOR_WRITE": True,
         },
