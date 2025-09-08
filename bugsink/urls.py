be7ac2043b4276412ba57e05568d1a9c037d25ae
Klaas van Schelven <klaas@vanschelven.com>
2025-09-08 14:50:23 +0200

    Implement /api/0/ endpoint
    Fix #97

--- bugsink/urls.py
+++ bugsink/urls.py
@@ -11,7 +11,7 @@ from teams.views import debug_email as debug_teams_email
 from bugsink.app_settings import get_settings
 from users.views import signup, confirm_email, resend_confirmation, request_reset_password, reset_password, preferences
 from ingest.views import download_envelope
-from files.views import chunk_upload, artifact_bundle_assemble, api_catch_all
+from files.views import chunk_upload, artifact_bundle_assemble, api_root, api_catch_all
 from bugsink.decorators import login_exempt
 
 from .views import home, trigger_error, favicon, settings_view, silence_email_system_warning, counts, health_check_ready
@@ -50,7 +50,7 @@ urlpatterns = [
     path("api/0/organizations/<slug:organization_slug>/artifactbundle/assemble/", artifact_bundle_assemble,
          name="artifact_bundle_assemble"),
 
-    path('api/', include('ingest.urls')),
+    path('api/0/', api_root, name='api_root'),
 
     path('api/<path:subpath>', api_catch_all, name='api_catch_all'),
 
