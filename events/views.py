17007d5f55eb3bf3ecfa8b1b647aa65f58670164
Klaas van Schelven <klaas@vanschelven.com>
2025-09-15 15:27:08 +0200

    MarkDown: in the regular UI too
    See #127

--- events/views.py
+++ events/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from bugsink.decorators import event_membership_required, atomic_for_request_method
 from issues.utils import get_values
 
+from .markdown_stacktrace import render_stacktrace_md
+
 
 @atomic_for_request_method
 @event_membership_required
@@ -25,3 +27,15 @@ def event_plaintext(request, event):
         "event": event,
         "exceptions": exceptions,
     }, content_type="text/plain")
+
+
+@atomic_for_request_method
+@event_membership_required
+def event_markdown(request, event, as_attachment=False):
+    text = render_stacktrace_md(event, frames="in_app", exceptions="last", include_locals=True)
+    result = HttpResponse(text, content_type="text/markdown; charset=utf-8")
+    if as_attachment:
+        result["Content-Disposition"] = content_disposition_header(
+            as_attachment=True, filename=event.id.hex + ".md"
+        )
+    return result
