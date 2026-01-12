bugsink/bugsink

commit 1eea9268a54c35c55f9cff2e83b376c4d0d44d6f

Optimization: Search on EvenTag without involving Event if possible
When searching by tag, there is no need to join with Event; especially when
just counting results or determining first/last digest_order (for navigation).

(For the above "no need" to be actually true, digest_order was denormalized
into EventTag).

The above is implemented in `search_events_optimized`.

Further improvements:

* the bounds of `digest_order` are fetched only once; for first/last this info
  is reused.

* explicitly pass `event_qs_count` to the templates

* non-event pages used to calculate a "last event" to generate a tab with a
  correct event.id; since we simply have the "last" idiom, better use that.
  this also makes clear the "none" idiom was never needed, we remove it again.

Results:

Locally (60K event DB, 30K events on largest issue) my testbatch now
runs in 25% of time (overall).

* The effect on the AND-ing are in fact very large (13% runtime remaining)
* The event details page is not noticably improved.

issues/views.py
@@ -24,7 +24,7 @@ from events.ua_stuff import get_contexts_enriched_with_ua
 
 from compat.timestamp import format_timestamp
 from projects.models import ProjectMembership
-from tags.search import search_issues, search_events
+from tags.search import search_issues, search_events, search_events_optimized
 
 from .models import Issue, IssueQuerysetStateManager, IssueStateManager, TurningPoint, TurningPointKind
 from .forms import CommentForm
@@ -287,42 +287,49 @@ def _handle_post(request, issue):
     return HttpResponseRedirect(request.path_info)
 
 
-def _get_event(qs, event_pk, digest_order, nav):
-    """Returns the event using the "url lookup"."""
+def _get_event(qs, issue, event_pk, digest_order, nav, bounds):
+    """
+    Returns the event using the "url lookup".
+    The passed qs is "something you can use to deduce digest_order (for next/prev)."
+    When a direct (non-nav) method is used, we do _not_ check against existence in qs; this is more performant, and it's
+    not clear that being pedantic in this case is actually more valuable from a UX perspective.
+    """
 
     if nav is not None:
         if nav not in ["first", "last", "prev", "next"]:
             raise Http404("Cannot look up with '%s'" % nav)
 
         if nav == "first":
-            result = qs.order_by("digest_order").first()
+            # basically, the below. But because first/last are calculated anyway for "_has_next_prev", we pass these
+            # digest_order = qs.order_by("digest_order").values_list("digest_order", flat=True).first()
+            digest_order = bounds[0]
         elif nav == "last":
-            result = qs.order_by("digest_order").last()
+            # digest_order = qs.order_by("digest_order").values_list("digest_order", flat=True).last()
+            digest_order = bounds[1]
         elif nav in ["prev", "next"]:
             if digest_order is None:
                 raise Http404("Cannot look up with '%s' without digest_order" % nav)
 
             if nav == "prev":
-                result = qs.filter(digest_order__lt=digest_order).order_by("-digest_order").first()
+                digest_order = qs.filter(digest_order__lt=digest_order).values_list("digest_order", flat=True)\
+                    .order_by("-digest_order").first()
             elif nav == "next":
-                result = qs.filter(digest_order__gt=digest_order).order_by("digest_order").first()
+                digest_order = qs.filter(digest_order__gt=digest_order).values_list("digest_order", flat=True)\
+                    .order_by("digest_order").first()
 
-        if result is None:
+        if digest_order is None:
             raise Event.DoesNotExist
-        return result
+        return Event.objects.get(issue=issue, digest_order=digest_order)
 
     elif event_pk is not None:
         # we match on both internal and external id, trying internal first
-        if event_pk == "none":
-            raise Event.DoesNotExist()
-
         try:
             return Event.objects.get(pk=event_pk)
         except Event.DoesNotExist:
-            return qs.get(event_id=event_pk)
+            return Event.objects.get(event_id=event_pk)
 
     elif digest_order is not None:
-        return qs.get(digest_order=digest_order)
+        return Event.objects.get(digest_order=digest_order)
     else:
         raise Http404("Either event_pk, nav, or digest_order must be provided")
 
@@ -333,12 +340,13 @@ def issue_event_stacktrace(request, issue, event_pk=None, digest_order=None, nav
     if request.method == "POST":
         return _handle_post(request, issue)
 
-    event_qs = search_events(issue.project, issue, request.GET.get("q", ""))
+    event_x_qs = search_events_optimized(issue.project, issue, request.GET.get("q", ""))
+    first_do, last_do = _first_last(event_x_qs)
 
     try:
-        event = _get_event(event_qs, event_pk, digest_order, nav)
+        event = _get_event(event_x_qs, issue, event_pk, digest_order, nav, (first_do, last_do))
     except Event.DoesNotExist:
-        return issue_event_404(request, issue, event_qs, "stacktrace", "event_stacktrace")
+        return issue_event_404(request, issue, event_x_qs, "stacktrace", "event_stacktrace")
 
     parsed_data = event.get_parsed_data()
 
@@ -387,26 +395,24 @@ def issue_event_stacktrace(request, issue, event_pk=None, digest_order=None, nav
         "stack_of_plates": stack_of_plates,
         "mute_options": GLOBAL_MUTE_OPTIONS,
         "q": request.GET.get("q", ""),
-        "event_qs": event_qs,
-        **_has_next_prev(event, event_qs),
+        "event_qs_count": event_x_qs.count(),
+        "has_prev": event.digest_order > first_do,
+        "has_next": event.digest_order < last_do,
     })
 
 
-def issue_event_404(request, issue, event_qs, tab, this_view):
+def issue_event_404(request, issue, event_x_qs, tab, this_view):
     """If the Event is 404, but the issue is not, we can still show the issue page; we show a message for the event"""
 
-    last_event = event_qs.order_by("digest_order").last()  # used for switching to an event-page (using tabs)
     return render(request, "issues/event_404.html", {
         "tab": tab,
         "this_view": this_view,
         "project": issue.project,
         "issue": issue,
-        "event": last_event,
         "is_event_page": False,  # this variable is used to denote "we have event-related info", which we don't
         "mute_options": GLOBAL_MUTE_OPTIONS,
-        "event_qs": event_qs,
         "q": request.GET.get("q", ""),
-        "event_qs_count": event_qs.count(),  # avoids repeating the count() query
+        "event_qs_count": event_x_qs.count(),
     })
 
 
@@ -416,12 +422,13 @@ def issue_event_breadcrumbs(request, issue, event_pk=None, digest_order=None, na
     if request.method == "POST":
         return _handle_post(request, issue)
 
-    event_qs = search_events(issue.project, issue, request.GET.get("q", ""))
+    event_x_qs = search_events_optimized(issue.project, issue, request.GET.get("q", ""))
+    first_do, last_do = _first_last(event_x_qs)
 
     try:
-        event = _get_event(event_qs, event_pk, digest_order, nav)
+        event = _get_event(event_x_qs, issue, event_pk, digest_order, nav, (first_do, last_do))
     except Event.DoesNotExist:
-        return issue_event_404(request, issue, event_qs, "breadcrumbs", "event_breadcrumbs")
+        return issue_event_404(request, issue, event_x_qs, "breadcrumbs", "event_breadcrumbs")
 
     parsed_data = event.get_parsed_data()
 
@@ -436,8 +443,9 @@ def issue_event_breadcrumbs(request, issue, event_pk=None, digest_order=None, na
         "breadcrumbs": get_values(parsed_data.get("breadcrumbs")),
         "mute_options": GLOBAL_MUTE_OPTIONS,
         "q": request.GET.get("q", ""),
-        "event_qs": event_qs,
-        **_has_next_prev(event, event_qs),
+        "event_qs_count": event_x_qs.count(),
+        "has_prev": event.digest_order > first_do,
+        "has_next": event.digest_order < last_do,
     })
 
 
@@ -447,18 +455,11 @@ def _date_with_milis_html(timestamp):
         '<span class="text-xs">' + date(timestamp, "u")[:3] + '</span>')
 
 
-def _has_next_prev(event, event_qs):
-    # guarding against empty event_qs is not necessary, because we only get here if any event exists (because otherwise
-    # event would be None too)
-
-    # this was once implemented with Min/Max, but just doing 2 queries is (on sqlite at least) ~100× faster.
-    first = event_qs.order_by("digest_order").values("digest_order").first()
-    last = event_qs.order_by("-digest_order").values("digest_order").first()
-
-    return {
-        "has_prev": event.digest_order > first["digest_order"],
-        "has_next": event.digest_order < last["digest_order"],
-    }
+def _first_last(qs_with_digest_order):
+    # this was once implemented with Min/Max, but just doing 2 queries is (on sqlite at least) much faster.
+    first = qs_with_digest_order.order_by("digest_order").values_list("digest_order", flat=True).first()
+    last = qs_with_digest_order.order_by("-digest_order").values_list("digest_order", flat=True).first()
+    return first, last
 
 
 @atomic_for_request_method
@@ -467,12 +468,13 @@ def issue_event_details(request, issue, event_pk=None, digest_order=None, nav=No
     if request.method == "POST":
         return _handle_post(request, issue)
 
-    event_qs = search_events(issue.project, issue, request.GET.get("q", ""))
+    event_x_qs = search_events_optimized(issue.project, issue, request.GET.get("q", ""))
+    first_do, last_do = _first_last(event_x_qs)
 
     try:
-        event = _get_event(event_qs, event_pk, digest_order, nav)
+        event = _get_event(event_x_qs, issue, event_pk, digest_order, nav, (first_do, last_do))
     except Event.DoesNotExist:
-        return issue_event_404(request, issue, event_qs, "event-details", "event_details")
+        return issue_event_404(request, issue, event_x_qs, "event-details", "event_details")
     parsed_data = event.get_parsed_data()
 
     key_info = [
@@ -560,8 +562,9 @@ def issue_event_details(request, issue, event_pk=None, digest_order=None, nav=No
         "contexts": contexts,
         "mute_options": GLOBAL_MUTE_OPTIONS,
         "q": request.GET.get("q", ""),
-        "event_qs": event_qs,
-        **_has_next_prev(event, event_qs),
+        "event_qs_count": event_x_qs.count(),
+        "has_prev": event.digest_order > first_do,
+        "has_next": event.digest_order < last_do,
     })
 
 
@@ -572,12 +575,11 @@ def issue_history(request, issue):
         return _handle_post(request, issue)
 
     event_qs = search_events(issue.project, issue, request.GET.get("q", ""))
-    last_event = event_qs.order_by("digest_order").last()  # used for switching to an event-page (using tabs)
+    last_event = event_qs.order_by("digest_order").last()
     return render(request, "issues/history.html", {
         "tab": "history",
         "project": issue.project,
         "issue": issue,
-        "event": last_event,
         "is_event_page": False,
         "request_repr": _request_repr(last_event.get_parsed_data()) if last_event is not None else "",
         "mute_options": GLOBAL_MUTE_OPTIONS,
@@ -591,12 +593,11 @@ def issue_tags(request, issue):
         return _handle_post(request, issue)
 
     event_qs = search_events(issue.project, issue, request.GET.get("q", ""))
-    last_event = event_qs.order_by("digest_order").last()  # used for switching to an event-page (using tabs)
+    last_event = event_qs.order_by("digest_order").last()
     return render(request, "issues/tags.html", {
         "tab": "tags",
         "project": issue.project,
         "issue": issue,
-        "event": last_event,
         "is_event_page": False,
         "request_repr": _request_repr(last_event.get_parsed_data()) if last_event is not None else "",
         "mute_options": GLOBAL_MUTE_OPTIONS,
@@ -610,12 +611,11 @@ def issue_grouping(request, issue):
         return _handle_post(request, issue)
 
     event_qs = search_events(issue.project, issue, request.GET.get("q", ""))
-    last_event = event_qs.order_by("digest_order").last()  # used for switching to an event-page (using tabs)
+    last_event = event_qs.order_by("digest_order").last()
     return render(request, "issues/grouping.html", {
         "tab": "grouping",
         "project": issue.project,
         "issue": issue,
-        "event": last_event,
         "is_event_page": False,
         "request_repr": _request_repr(last_event.get_parsed_data()) if last_event is not None else "",
         "mute_options": GLOBAL_MUTE_OPTIONS,
@@ -628,9 +628,12 @@ def issue_event_list(request, issue):
     if request.method == "POST":
         return _handle_post(request, issue)
 
+    # because we we need _actual events_ for display, and we don't have the regular has_prev/has_next (paginator
+    # instead), we don't try to optimize using search_events_optimized in this view (except for counting)
     if "q" in request.GET:
         event_list = search_events(issue.project, issue, request.GET["q"]).order_by("digest_order")
-        paginator = Paginator(event_list, 250)  # might as well use Paginator; the cost of .count() is incurred anyway
+        event_x_qs = search_events_optimized(issue.project, issue, request.GET.get("q", ""))
+        paginator = KnownCountPaginator(event_list, 250, count=event_x_qs.count())
     else:
         event_list = issue.event_set.order_by("digest_order")
         # re 250: in general "big is good" because it allows a lot "at a glance".
@@ -639,13 +642,12 @@ def issue_event_list(request, issue):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    last_event = event_list.last()  # used for switching to an event-page (using tabs)
+    last_event = event_list.last()
 
     return render(request, "issues/event_list.html", {
         "tab": "event-list",
         "project": issue.project,
         "issue": issue,
-        "event": last_event,
         "event_list": event_list,
         "is_event_page": False,
         "request_repr": _request_repr(last_event.get_parsed_data()) if last_event is not None else "",

issues/templates/issues/base.html
@@ -107,9 +107,9 @@
     {# overflow-x-auto is needed at the level of the flex item such that it works at the level where we need it (the code listings)#}
     <div class="ml-4 mb-4 mr-4 border-2 overflow-x-auto flex-[2_1_96rem]"><!-- the whole of the big tabbed view--> {# 96rem is 1536px, which matches the 2xl class; this is no "must" but eyeballing revealed: good result #}
         <div class="flex bg-slate-50 border-b-2"><!-- container for the actual tab buttons -->
-            <a href="/issues/issue/{{ issue.id }}/event/{% if event %}{{ event.id }}{% else %}none{% endif %}/{% current_qs %}"><div class="p-4 font-bold hover:bg-slate-200 {% if tab == "stacktrace" %}text-cyan-500 border-cyan-500 border-b-4{% else %}text-slate-500 border-slate-400 hover:border-b-4{% endif %}">Stacktrace</div></a>
-            <a href="/issues/issue/{{ issue.id }}/event/{% if event %}{{ event.id }}{% else %}none{% endif %}/details/{% current_qs %}"><div class="p-4 font-bold hover:bg-slate-200 {% if tab == "event-details" %}text-cyan-500 border-cyan-500 border-b-4{% else %}text-slate-500 border-slate-400 hover:border-b-4{% endif %}">Event&nbsp;Details</div></a>
-            <a href="/issues/issue/{{ issue.id }}/event/{% if event %}{{ event.id }}{% else %}none{% endif %}/breadcrumbs/{% current_qs %}"><div class="p-4 font-bold hover:bg-slate-200 {% if tab == "breadcrumbs" %}text-cyan-500 border-cyan-500 border-b-4{% else %}text-slate-500 border-slate-400 hover:border-b-4{% endif %}">Breadcrumbs</div></a>
+            <a href="/issues/issue/{{ issue.id }}/event/{% if event %}{{ event.id }}{% else %}last{% endif %}/{% current_qs %}"><div class="p-4 font-bold hover:bg-slate-200 {% if tab == "stacktrace" %}text-cyan-500 border-cyan-500 border-b-4{% else %}text-slate-500 border-slate-400 hover:border-b-4{% endif %}">Stacktrace</div></a>
+            <a href="/issues/issue/{{ issue.id }}/event/{% if event %}{{ event.id }}{% else %}last{% endif %}/details/{% current_qs %}"><div class="p-4 font-bold hover:bg-slate-200 {% if tab == "event-details" %}text-cyan-500 border-cyan-500 border-b-4{% else %}text-slate-500 border-slate-400 hover:border-b-4{% endif %}">Event&nbsp;Details</div></a>
+            <a href="/issues/issue/{{ issue.id }}/event/{% if event %}{{ event.id }}{% else %}last{% endif %}/breadcrumbs/{% current_qs %}"><div class="p-4 font-bold hover:bg-slate-200 {% if tab == "breadcrumbs" %}text-cyan-500 border-cyan-500 border-b-4{% else %}text-slate-500 border-slate-400 hover:border-b-4{% endif %}">Breadcrumbs</div></a>
             <a href="/issues/issue/{{ issue.id }}/events/{% current_qs %}"><div class="p-4 font-bold hover:bg-slate-200 {% if tab == "event-list" %}text-cyan-500 border-cyan-500 border-b-4{% else %}text-slate-500 border-slate-400 hover:border-b-4{% endif %}">Event&nbsp;List</div></a>
             <a href="/issues/issue/{{ issue.id }}/tags/{% current_qs %}"><div class="p-4 font-bold hover:bg-slate-200 {% if tab == "tags" %}text-cyan-500 border-cyan-500 border-b-4{% else %}text-slate-500 border-slate-400 hover:border-b-4{% endif %}">Tags</div></a>
             <a href="/issues/issue/{{ issue.id }}/grouping/{% current_qs %}"><div class="p-4 font-bold hover:bg-slate-200 {% if tab == "grouping" %}text-cyan-500 border-cyan-500 border-b-4{% else %}text-slate-500 border-slate-400 hover:border-b-4{% endif %}">Grouping</div></a>

issues/templates/issues/breadcrumbs.html
@@ -8,7 +8,7 @@
 
 <div class="flex">
     <div class="overflow-hidden">
-        <div class="italic">{{ event.ingested_at|date:"j M G:i T" }} (Event {{ event.digest_order|intcomma }} of {{ issue.digested_event_count|intcomma }} total{% if q %} — {{ event_qs.count }} found by search{% endif %})</div>
+        <div class="italic">{{ event.ingested_at|date:"j M G:i T" }} (Event {{ event.digest_order|intcomma }} of {{ issue.digested_event_count|intcomma }} total{% if q %} — {{ event_qs_count }} found by search{% endif %})</div>
     </div>
 
     <div class="ml-auto flex-none">

issues/templates/issues/event_details.html
@@ -7,7 +7,7 @@
 
 <div class="flex">
     <div class="overflow-hidden">
-        <div class="italic">{{ event.ingested_at|date:"j M G:i T" }} (Event {{ event.digest_order|intcomma }} of {{ issue.digested_event_count|intcomma }} total{% if q %} — {{ event_qs.count }} found by search{% endif %})</div>
+        <div class="italic">{{ event.ingested_at|date:"j M G:i T" }} (Event {{ event.digest_order|intcomma }} of {{ issue.digested_event_count|intcomma }} total{% if q %} — {{ event_qs_count }} found by search{% endif %})</div>
     </div>
 
     <div class="ml-auto flex-none">

issues/templates/issues/stacktrace.html
@@ -10,7 +10,7 @@
     {# event-nav only #}
     <div class="flex">
         <div class="overflow-hidden">
-            <div class="italic">{{ event.ingested_at|date:"j M G:i T" }} (Event {{ event.digest_order|intcomma }} of {{ issue.digested_event_count|intcomma }} total{% if q %} — {{ event_qs.count }} found by search{% endif %})</div>
+            <div class="italic">{{ event.ingested_at|date:"j M G:i T" }} (Event {{ event.digest_order|intcomma }} of {{ issue.digested_event_count|intcomma }} total{% if q %} — {{ event_qs_count }} found by search{% endif %})</div>
         </div>
 
         <div class="ml-auto flex-none">
@@ -31,7 +31,7 @@
     <div class="flex">
         <div class="overflow-hidden">
             {% if forloop.counter0 == 0 %}
-            <div class="italic">{{ event.ingested_at|date:"j M G:i T" }} (Event {{ event.digest_order|intcomma }} of {{ issue.digested_event_count|intcomma }} total{% if q %} — {{ event_qs.count }} found by search{% endif %})</div>
+            <div class="italic">{{ event.ingested_at|date:"j M G:i T" }} (Event {{ event.digest_order|intcomma }} of {{ issue.digested_event_count|intcomma }} total{% if q %} — {{ event_qs_count }} found by search{% endif %})</div>
             {% endif %}
             <h1 class="text-2xl font-bold {% if forloop.counter0 > 0 %}mt-4{% endif %} text-ellipsis whitespace-nowrap overflow-hidden">{{ exception.type }}</h1>
             <div class="text-lg mb-4 text-ellipsis whitespace-nowrap overflow-hidden">{{ exception.value }}</div>

issues/urls.py
@@ -22,7 +22,6 @@ def regex_converter(passed_regex):
 
 register_converter(regex_converter("(first|last)"), "first-last")
 register_converter(regex_converter("(prev|next)"), "prev-next")
-register_converter(regex_converter("(none)"), "str-none")
 
 
 urlpatterns = [
@@ -35,11 +34,6 @@ urlpatterns = [
     path('issue/<uuid:issue_pk>/event/<uuid:event_pk>/', issue_event_stacktrace, name="event_stacktrace"),
     path('issue/<uuid:issue_pk>/event/<uuid:event_pk>/details/', issue_event_details, name="event_details"),
     path('issue/<uuid:issue_pk>/event/<uuid:event_pk>/breadcrumbs/', issue_event_breadcrumbs, name="event_breadcrumbs"),
-
-    path('issue/<uuid:issue_pk>/event/<str-none:event_pk>/', issue_event_stacktrace, name="event_stacktrace"),
-    path('issue/<uuid:issue_pk>/event/<str-none:event_pk>/details/', issue_event_details, name="event_details"),
-    path('issue/<uuid:issue_pk>/event/<str-none:event_pk>/breadcrumbs/',
-         issue_event_breadcrumbs, name="event_breadcrumbs"),
 
     path('issue/<uuid:issue_pk>/event/<int:digest_order>/', issue_event_stacktrace, name="event_stacktrace"),
     path('issue/<uuid:issue_pk>/event/<int:digest_order>/details/', issue_event_details, name="event_details"),

performance/management/commands/pftest_search.py
@@ -61,7 +61,7 @@ def _format_query_plan(rows):
 
 
 @contextmanager
-def query_debugger():
+def query_debugger(print_all):
     d = {}
     queries_i = len(connection.queries)
     yield d
@@ -70,7 +70,10 @@ def query_debugger():
     print('Queries executed:', len(connection.queries) - queries_i)
     print('Total query time:', sum(float(query['time']) for query in connection.queries[queries_i:]))
 
-    interesting_queries = [query for query in connection.queries[queries_i:] if float(query['time']) > 0.005]
+    if print_all:
+        interesting_queries = connection.queries[queries_i:]
+    else:
+        interesting_queries = [query for query in connection.queries[queries_i:] if float(query['time']) > 0.005]
 
     for query in interesting_queries:
         print()
@@ -89,7 +92,7 @@ def query_debugger():
 class Command(BaseCommand):
     """Internal (debugging) command to test the performance of search queries."""
 
-    def _test_url(self, url, title, description):
+    def _test_url(self, url, title, description, print_all=False):
         """
         Runs the view code that matches the given URL with a fake request; prints relevant stats
         """
@@ -105,7 +108,7 @@ class Command(BaseCommand):
         print(description)
         print()
 
-        with query_debugger() as d:
+        with query_debugger(print_all) as d:
             view(request, *args, **kwargs)
 
         self.total_time += d['total_time']

tags/migrations/0002_remove_eventtag_tags_eventt_value_i_255b9c_idx_and_more.py
@@ -1,0 +1,30 @@
+# Generated by Django 4.2.19 on 2025-03-12 13:46
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("tags", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RemoveIndex(
+            model_name="eventtag",
+            name="tags_eventt_value_i_255b9c_idx",
+        ),
+        migrations.AddField(
+            model_name="eventtag",
+            name="digest_order",
+            field=models.PositiveIntegerField(default=0),
+            preserve_default=False,
+        ),
+        migrations.AddIndex(
+            model_name="eventtag",
+            index=models.Index(
+                fields=["value", "issue", "digest_order"],
+                name="tags_eventt_value_i_6f1823_idx",
+            ),
+        ),
+    ]

commit ee52ef50eb24f663239525d15db8d1dabe5a3046

Fix broken 'lookup by issue-id and digest_order' in URL bar
(this wasn't very obvious since the URL was only there for 'ergonomics' to
begin with)

broken in 1eea9268

issues/views.py
@@ -423,7 +423,8 @@ def _get_event(qs, issue, event_pk, digest_order, nav, bounds):
             return Event.objects.get(project=issue.project, event_id=event_pk)
 
     elif digest_order is not None:
-        return Event.objects.get(digest_order=digest_order)
+        # "ergonomics" when people type this in the URL bar
+        return qs.get(digest_order=digest_order)
     else:
         raise Http404("Either event_pk, nav, or digest_order must be provided")
 