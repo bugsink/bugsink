0d55bda834be9ba5f137cd27a8581a89009cd8d5
Klaas van Schelven <klaas@vanschelven.com>
2026-01-19 15:36:49 +0100

    ee/tenants: 503 for stopping/migrating

--- ee/tenants/middleware.py
+++ ee/tenants/middleware.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.shortcuts import render
+
 from .base import use_tenant_subdomain
 
 
@@ -9,6 +12,16 @@ class SelectDatabaseMiddleware:
     def __call__(self, request):
         host = request.get_host()
         request_subdomain = host.split('.')[0].split(':')[0]
+
+        if not request.path.startswith("/static/") and request_subdomain not in settings.TENANTS:
+            if hasattr(settings, "TENANTS_STOPPING") and request_subdomain in settings.TENANTS_STOPPING:
+                message = "This Hosted Bugsink is no longer available."
+            elif hasattr(settings, "TENANTS_MAINTENANCE") and request_subdomain in settings.TENANTS_MAINTENANCE:
+                message = "Your Hosted Bugsink is being upgraded. Check again in a moment."
+            else:
+                message = "Service Unavailable"
+
+            return render(request, "503.html", {"message": message}, status=503)
 
         with use_tenant_subdomain(request_subdomain):
             response = self.get_response(request)
