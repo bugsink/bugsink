1a5209109afa7198fa767429d550a101020b3098
Klaas van Schelven <klaas@vanschelven.com>
2026-01-26 20:54:50 +0100

    fix_project_digest_order: add dry-run
    and do more in-memory, allowing us to not-touch existing
    digested_at data

--- events/management/commands/fix_project_digest_order.py
+++ events/management/commands/fix_project_digest_order.py
@@ -1,7 +1,7 @@
 import datetime
 from django.core.management.base import BaseCommand
 
-from bugsink.transaction import immediate_atomic
+from bugsink.transaction import immediate_atomic, durable_atomic
 
 from projects.models import Project
 from events.models import Event
@@ -10,7 +10,7 @@ from events.models import Event
 class Command(BaseCommand):
     help = """Set the project_digest_order field on all Events that lack it; as a command, since it may be expensive."""
 
-    def fix_project_digest_order_for_project(self, project):
+    def fix_project_digest_order_for_project(self, project, dry_run=False):
         # this script makes a best-effort guess at project_digest_order; best-effort, because in the light of evictions
         # such information can not be fully reconstructed. What we _do_ know: issue-level digest_order. We use this info
         # to establish a lower bound (going up through all digested events) and upper bound (going down) and then
@@ -22,32 +22,26 @@ class Command(BaseCommand):
         results_going_up = {}
         results_going_down = {}
 
-        # Re-monotomize:
+        # Re-monotomize (in-memory) digested_at per issue
         seen_digested_ats = {}
-        for d in (Event.objects.filter(project=project, project_digest_order__isnull=True)
-                  .order_by("issue_id", "digest_order")
-                  .values("id", "digested_at", "digest_order", "issue_id")):
+        events = [d for d in (Event.objects.filter(project=project, project_digest_order__isnull=True)
+                              .order_by("issue_id", "digest_order")
+                              .values("id", "digested_at", "digest_order", "issue_id"))]
 
+        for d in events:
             if d["issue_id"] not in seen_digested_ats:
                 seen_digested_ats[d["issue_id"]] = datetime.datetime.min.replace(tzinfo=d["digested_at"].tzinfo)
 
             if d["digested_at"] < seen_digested_ats[d["issue_id"]]:
-                time_delta = seen_digested_ats[d["issue_id"]] - d["digested_at"]
-                print(
-                    f"Re-monotomizing digested_at for event {d['id']} to "
-                    f"{seen_digested_ats[d['issue_id']]} (was {d['digested_at']}, delta {time_delta})")
-
-                Event.objects.filter(id=d["id"]).update(digested_at=seen_digested_ats[d["issue_id"]])
+                d["digested_at"] = seen_digested_ats[d["issue_id"]]
             else:
                 seen_digested_ats[d["issue_id"]] = d["digested_at"]
 
+        events = sorted(events, key=lambda x: (x["digested_at"], x["digest_order"]))
+
         # UP
         current = 0
-        for d in (
-                Event.objects.filter(project=project, project_digest_order__isnull=True)
-                .order_by("digested_at", "digest_order")
-                .values("id", "digest_order", "issue_id")):
-
+        for d in events:
             if d["issue_id"] not in by_issue_going_up:
                 by_issue_going_up[d["issue_id"]] = 0
 
@@ -65,11 +59,7 @@ class Command(BaseCommand):
         else:
             current = project.digested_event_count + 1
 
-        for d in (
-                Event.objects.filter(project=project, project_digest_order__isnull=True)
-                .order_by("-digested_at", "-digest_order")
-                .values("id", "digest_order", "issue_id")):
-
+        for d in reversed(events):
             if d["issue_id"] not in by_issue_going_down:
                 by_issue_going_down[d["issue_id"]] = d["digest_order"] + 1  # +1 for "the next unseen"
 
@@ -96,12 +86,26 @@ class Command(BaseCommand):
             prev = picked
             assert picked >= lo and picked <= hi, (lo, hi, picked)
 
-            Event.objects.filter(id=k).update(project_digest_order=picked)
+            if not dry_run:
+                Event.objects.filter(id=k).update(project_digest_order=picked)
+            else:
+                self.stdout.write(f"  {k} project_digest_order := {picked}")
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--dry-run",
+            action="store_true",
+            help="Perform a dry run without making any changes.",
+        )
 
     def handle(self, *args, **options):
+        dry_run = options.get("dry_run", False)
+
         for project in Project.objects.all():
             self.stdout.write(f"Fixing project_digest_order for project {project.id} ({project.name})")
 
+            relevant_atomic = immediate_atomic if not dry_run else durable_atomic
+
             # global lock given the complexity of what we do (won't work e.g. with simultaneous evictions)
-            with immediate_atomic():
-                self.fix_project_digest_order_for_project(project)
+            with relevant_atomic():
+                self.fix_project_digest_order_for_project(project, dry_run=dry_run)
