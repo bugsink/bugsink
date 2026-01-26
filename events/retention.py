bugsink/bugsink

commit 4130cd2402055ae184596942b29a7527621cfd9d

cleanup_events (after delete): push out of transaction
I ran into a situation where an eviction-containing transaction would
be rolled back. In that situation the underlying events would have
been deleted from the storage the next time the eviction was done,
causing the logs to get spammed.

Solution: only remove the files once the transaction is actually
committed.

I had a vague memory that I thought about this earlier and didn't do
it at the time "for reasons" but I can't find those reasons in the
git-log/comments, the tests (hitting this code) work just fine; and
running the server in both EAGER/Non-EAGER modes works too so I'm
going to just push this forward.

Note: we use regular on_commit logic; no need to push this into snappea
(it's going to be fast enough, and pushing 500 uuids into snappea's
task description is not what we engineered snappea for).

events/retention.py
@@ -1,5 +1,7 @@
+from functools import partial
 import logging
 from django.db.models import Q, Min, Max, Count
+from django.db import transaction
 
 from datetime import timezone, datetime
 
@@ -393,6 +395,10 @@ def evict_for_epoch_and_irrelevance(project, max_epoch, max_irrelevance, max_eve
 
 
 def cleanup_events_on_storage(todos):
+    transaction.on_commit(partial(_cleanup_events_on_storage, todos))
+
+
+def _cleanup_events_on_storage(todos):
     for event_id, storage_backend in todos:
         try:
             get_storage(storage_backend).delete(event_id)

commit 0f144af0b46fa1a42b22b28f550523e4898b797f

Fix test for delayed cleanup_events_on_storage
as per 7f726cad8fc9 a fix was needed, but the bug introduced in
4130cd240205 was never surfaced by the tests. The present tests
_do_ fail for the bug

events/retention.py
@@ -395,7 +395,7 @@ def evict_for_epoch_and_irrelevance(project, max_epoch, max_irrelevance, max_eve
 
 
 def cleanup_events_on_storage(todos):
-    todos = list(todos)  # force evaluation now
+    todos = list(todos)  # force evaluation _inside_ the transaction (on_commit the todos will be gone otherwise)
     transaction.on_commit(partial(_cleanup_events_on_storage, todos))
 
 

ingest/tests.py
@@ -17,6 +17,7 @@ from django.test.client import RequestFactory
 from django.core.exceptions import ValidationError
 
 from bugsink.test_utils import TransactionTestCase25251 as TransactionTestCase
+from bugsink.transaction import immediate_atomic
 from projects.models import Project
 from events.factories import create_event_data, create_event
 from events.retention import evict_for_max_events
@@ -595,9 +596,10 @@ class IngestViewTestCase(TransactionTestCase):
                 [e.get_parsed_data() for e in Event.objects.all()]  # force reading from filestore
                 self.assertEqual(len(os.listdir(tempdir)), 2)  # test_envelope_endpoint creates 2 events
 
-                project = Project.objects.get(name="test")
-                project.retention_max_event_count = 1
-                evict_for_max_events(project, timezone.now(), stored_event_count=2)
+                with immediate_atomic():
+                    project = Project.objects.get(name="test")
+                    project.retention_max_event_count = 1
+                    evict_for_max_events(project, timezone.now(), stored_event_count=2)
 
                 self.assertEqual(len(os.listdir(tempdir)), 1)  # we set the max to 1, so one should remain
 
@@ -617,9 +619,10 @@ class IngestViewTestCase(TransactionTestCase):
                 [e.get_parsed_data() for e in Event.objects.all()]  # force reading from filestore
                 self.assertEqual(len(os.listdir(tempdir)), 2)  # test_envelope_endpoint creates 2 events
 
-                project = Project.objects.get(name="test")
-                project.retention_max_event_count = 1
-                evict_for_max_events(project, timezone.now(), stored_event_count=2)
+                with immediate_atomic():
+                    project = Project.objects.get(name="test")
+                    project.retention_max_event_count = 1
+                    evict_for_max_events(project, timezone.now(), stored_event_count=2)
 
                 self.assertEqual(len(os.listdir(tempdir)), 1)  # we set the max to 1, so one should remain
 
@@ -641,9 +644,10 @@ class IngestViewTestCase(TransactionTestCase):
                 [e.get_parsed_data() for e in Event.objects.all()]  # force reading from filestore
                 self.assertEqual(len(os.listdir(tempdir)), 2)  # test_envelope_endpoint creates 2 events
 
-                project = Project.objects.get(name="test")
-                project.retention_max_event_count = 1
-                evict_for_max_events(project, timezone.now(), stored_event_count=2)
+                with immediate_atomic():
+                    project = Project.objects.get(name="test")
+                    project.retention_max_event_count = 1
+                    evict_for_max_events(project, timezone.now(), stored_event_count=2)
 
                 self.assertEqual(len(os.listdir(tempdir)), 1)  # we set the max to 1, so one should remain
 
@@ -665,9 +669,10 @@ class IngestViewTestCase(TransactionTestCase):
                 [e.get_parsed_data() for e in Event.objects.all()]  # force reading from filestore
                 self.assertEqual(len(os.listdir(tempdir)), 2)  # test_envelope_endpoint creates 2 events
 
-                project = Project.objects.get(name="test")
-                project.retention_max_event_count = 1
-                evict_for_max_events(project, timezone.now(), stored_event_count=2)
+                with immediate_atomic():
+                    project = Project.objects.get(name="test")
+                    project.retention_max_event_count = 1
+                    evict_for_max_events(project, timezone.now(), stored_event_count=2)
 
                 self.assertEqual(len(os.listdir(tempdir)), 1)  # we set the max to 1, so one should remain
 